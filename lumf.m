c3b6ca6418602be47d49a8c2906bf1b310c7c568
Laszlo Dobos <dobos@complex.elte.hu>
2014-12-01 23:28:40 +0100

    Luminosity function replaced with the three-parameter version.

--- lumf.m
+++ lumf.m
@@ -3,5 +3,5 @@ function phi = lumf(beta, l, u, x)
 % Calculates the value of the luminosity function
 %--------------------------------------------------------------
 
-%phi = (x ./ u) .^ beta .* (1 - exp(-x ./ l)) .* exp(-x ./ u);
-phi = (x ./ u) .^ beta .* exp(-x ./ u);
+phi = (x ./ u) .^ beta .* (1 - exp(-x ./ l)) .* exp(-x ./ u);
+%phi = (x ./ u) .^ beta .* exp(-x ./ u);
