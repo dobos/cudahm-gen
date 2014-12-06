02535908d621d17bc8968fca8af4b08c61af43d3
Laszlo Dobos <dobos@complex.elte.hu>
2014-12-07 00:37:16 +0100

    Added run script to generate all the data and plots.

--- lumf.m
+++ lumf.m
@@ -3,5 +3,11 @@ function phi = lumf(beta, l, u, x)
 % Calculates the value of the luminosity function
 %--------------------------------------------------------------
 
+% Low cut-off version
 phi = (x ./ u) .^ beta .* (1 - exp(-x ./ l)) .* exp(-x ./ u);
+
+% Log version
+%phi = beta .* log(x ./ u) - x ./ u + log(1 - exp(-x ./ l));
+
+% Original Schechter form
 %phi = (x ./ u) .^ beta .* exp(-x ./ u);
--- luminosities.m
+++ luminosities.m
@@ -1,14 +1,15 @@
-function lm = luminosities(beta, lmag, umag, mag1, mag2, n)
+function lm = luminosities(n, beta, lmag, umag, m1, m2)
 %--------------------------------------------------------------
 % Generates a random sample based on the luminosity function
 %--------------------------------------------------------------
 
-% Convert mags to luminosity
 l = mag2lum(lmag);
 u = mag2lum(umag);
-l1 = mag2lum(mag1);
-l2 = mag2lum(mag2);
-plmax = lumf(beta, l, u, l1);   % max at faint end
+l1 = mag2lum(m1);
+l2 = mag2lum(m2);
+
+% Convert mags to luminosity
+plmax = lumf(beta, l, u, mag2lum(m1));   % max at faint end
 
 % Preallocate
 lm = zeros([n, 1]);
@@ -16,12 +17,18 @@ lm = zeros([n, 1]);
 datestr(now, 'HH:MM:SS')
 
 for i = 1:n
-    % Pick a luminosity between l1 and l2
+
     y = 1;
     p = 0;
     
     while y > p
+        % Pick a luminosity between l1 and l2
         rl = l1 + (l2 - l1) * rand();
+        
+        % Pick a magnitude between m1 and m2
+        %rm = m1 + (m2 - m1) * rand();
+        %rl = mag2lum(rm);
+        
         p = lumf(beta, l, u, rl);
         y = plmax * rand();
     end
